04de349b3fc7e5c7e420e3e44767b13bc858971c
Alexey Gribchenkov <gribchenkov@net-watch.ru>
2026-03-05 15:33:40 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,53 +1,55 @@
+import java.util.Random;
+public class Main
+{
+    public static final int SIZE = 8;
 
-public static final int SIZE = 8;
+    public static void main(String[] args) {
 
-void main() {
+        System.out.println("Крутим матрицы");
 
-    System.out.println("Крутим матрицы");
+        System.out.println("Исходная марица");
+        int[][] colors = new int[SIZE][SIZE];
 
-    System.out.println("Исходная марица");
-    int[][] colors = new int[SIZE][SIZE];
-
-    Random random = new Random();
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            colors[i][j] = random.nextInt(256);
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                colors[i][j] = random.nextInt(256);
+            }
         }
+
+        showMatrix(colors);
+
+        System.out.println("Повёрнутая матрица на 90 градусов");
+        showMatrix(rotateMatrix(colors));
+
     }
 
-    showMatrix(colors);
+    public static void showMatrix(int[][] colors) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.format("%4d", colors[i][j]);
+            }
+            System.out.println();
+        }
 
-    System.out.println("Повёрнутая матрица на 90 градусов");
-    showMatrix(rotateMatrix(colors));
 
+    }
+
+    public static int[][] rotateMatrix(int[][] colors) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = i + 1; j < SIZE; j++) {
+                int temp = colors[i][j];
+                colors[i][j] = colors[j][i];
+                colors[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE / 2; j++) {
+                int temp = colors[i][j];
+                colors[i][j] = colors[i][SIZE - 1 - j];
+                colors[i][SIZE - 1 - j] = temp;
+            }
+        }
+        return colors;
+    }
 }
-
-public static void showMatrix(int[][] colors) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            System.out.format("%4d", colors[i][j]);
-        }
-        System.out.println();
-    }
-
-
-}
-
-public static int[][] rotateMatrix(int[][] colors) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = i + 1; j < SIZE; j++) {
-            int temp = colors[i][j];
-            colors[i][j] = colors[j][i];
-            colors[j][i] = temp;
-        }
-    }
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE / 2; j++) {
-            int temp = colors[i][j];
-            colors[i][j] = colors[i][SIZE - 1 - j];
-            colors[i][SIZE - 1 - j] = temp;
-        }
-    }
-    return colors;
-}
-
